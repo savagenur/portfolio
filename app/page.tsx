ef7c218ee64103048db4675fcd4633fee195523b
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2025-08-31 01:06:03 -0700

    Add Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import Navbar from "./components/Navbar";
 
 export default function Home() {
-  return <>
-  <div className="text-5xl">
-    Whereas disregard and contempt for human rights have resulted</div></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
